4bfbf4d410f06f930fdf92b7172b0bce6cbda45c
mammoozz <muhammedpp807@gmail.com>
2026-06-08 06:12:55 +0530

    Create register.js

---
+++ instagram-reels-clone-blueprint/register.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCtBHVg3FI4C3i1WKl_iNpB9lS-xTOT73I",
+    authDomain: "reelgram-6636b.firebaseapp.com",
+    projectId: "reelgram-6636b",
+    storageBucket: "reelgram-6636b.firebasestorage.app",
+    messagingSenderId: "1019099169791",
+    appId: "1:1019099169791:web:520d4ad670a4c00bd2ddca"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
